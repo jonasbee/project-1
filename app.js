046bedcfaa9479a7f8efed50ae23f13ddc94b6aa
jonasbolduan@posteo.de <jonasbolduan@posteo.de>
2021-04-16 18:57:36 +0200

    fristDay changes

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
+// ! constructor
 const grid = document.querySelector('.grid')
+const startButton = document.querySelector('#start')
 const width = 4
 const cells = []
+let movement = false
+let possibility = true
+let arrayOfFreePos = []
+let randomIndex = 0
+let keyChoice = null
+let tileA = null
+let tileB = null
 
+// build grid
 for (let index = 0; index < width ** 2; index++) {
   const div = document.createElement('div')
   grid.appendChild(div)
@@ -12,94 +22,137 @@ for (let index = 0; index < width ** 2; index++) {
   cells.push(div)
 }
 
-// const startButton = document.querySelector(‘#start’)
-// startButton.addEventListener(‘click’, => {
-// <all code needed to run game>
-// })
+// ! functions
+
+// create array with all indexes of free positions
+function getFreeTileIndeces() {
+  arrayOfFreePos = []
+  cells.forEach((cell, index) => {
+    if (!cell.classList.contains('number')) {
+      arrayOfFreePos.push(index)
+    }
+  })
+}
+
+function checkIf2048() {
+  cells.forEach((cell) => {
+    if (cell.classList.contains('2048')) {
+      alert('Win')
+    }
+    return
+  })
+}
+
+// get new random number
+function getRandomIndexFromFreeCells() {
+  getFreeTileIndeces()
+  randomIndex = Math.floor(Math.random() * arrayOfFreePos.length)
+  return arrayOfFreePos[randomIndex]
+}
+
+function addTileRandomly(tileNumber) {
+  randomIndex = getRandomIndexFromFreeCells()
+  cells[randomIndex].classList.add('number')
+  cells[randomIndex].style.backgroundColor = 'green'
+  // cells[randomIndex].classList.add(`${tileNumber}`)
+  cells[randomIndex].innerHTML = `${tileNumber}`
+  console.dir(cells[randomIndex])
+}
+
+function compareTwoTilesForButtonUp(startIndex) {
+  // if boarder reached, return
+  if (!(startIndex > (width ** 2) - width - 1)) {
+    return
+  }
+  tileA = cells[startIndex]
+  tileB = cells[startIndex + 4]
+  // case two number tiles
+  if (tileA.classList.contains('number') && tileB.classList.contains('number')) {
+    if (tileA.innerHTML === tileB.innerHTML) {
+      tileB.classList.remove('number')
+      // no ${} needed, because innerHTML is type string?
+      // tileB.classList.remove(`${tileB.innerHTML}`)
+      tileB.style.backgroundColor = 'white'
+      tileB.innerHTML = ''
+      // tileA.classList.remove(`${tileA.innerHTML}`)
+      tileB.style.backgroundColor = 'white'
+
+      // TODO test here before running
+
+      tileA.classList.add(`${Number(tileA.innerHTML) * 2}`)
+      tileA.innerHTML = `${Number(tileA.innerHTML) * 2}`
+      movement = true
+      if (!(startIndex < width)) {
+        compareTwoTilesForButtonUp(startIndex - 4)
+        return
+      } else {
+        compareTwoTilesForButtonUp(startIndex)
+        return
+      }
+    } else {
+      return compareTwoTilesForButtonUp(startIndex + 4)
+    }
+    // case first is none, second is tile
+  } else if (!tileA.classList.contains('number') && tileB.classList.contains('number')) {
+    // tileA gets value of tileB
+    tileA.classList.add('number')
+    tileA.classList.add(`${tileB.innerHTML}`)
+    tileA.innerHTML = `${tileB.innerHTML}`
+    // tileB's value gets removed
+    tileB.classList.remove('number')
+    tileB.classList.remove(`${tileB.innerHTML}`)
+    tileB.innerHTML = ''
+    movement = true
+    if (!startIndex < width) {
+      return compareTwoTilesForButtonUp(startIndex - 4)
+    } else {
+      return compareTwoTilesForButtonUp(startIndex + 4)
+    }
+  }
+  // case tileA number, tileB none & tileA none, tileB none
+  else {
+    compareTwoTilesForButtonUp(startIndex + 4)
+  }
+}
+
+function moveUp() {
+  if (!possibility) {
+    alert('Game Over!')
+  }
+  // perform tile comparison for each column
+  for (let index = 0; index < width.length; index++) {
+    compareTwoTilesForButtonUp(index)
+  }
+  checkIf2048()
+  if (movement) {
+    getFreeTileIndeces()
+    if (arrayOfFreePos.length !== 0) {
+      addTileRandomly(2)
+    }
+    // reset movement
+    movement = false
+  } else {
+    return
+  }
+}
+
+// ! wrap whole execution in below
+startButton.addEventListener('click', (event) => {
+
+  // ! Game Start: randomly add two tile2
+  addTileRandomly(2)
+  addTileRandomly(2)
+
+  document.addEventListener('keydown', (event) => {
+    keyChoice = event.key
+    if (keyChoice === 'ArrowUp') {
+      moveUp()
+    }
+  })
+})
 
 // function seeIfThereArePossibilities()
 // for loop 4 times // for each key, change function (up, down, left, right)
 // for loop 4 times // for each column/row
 // arrayOfFunctions[index]
 // return possibility 
-
-// function checkIf2048()
-// cells.forEach( (cell) => {
-// if cell.classList.contains(‘#2048’) {
-// alert(Win)} 
-// })
-
-// // create array with all indexes of free positions
-// function getFreeTileIndexes()
-// arrayOfFreePos = cells.filter( (cell) =>  {
-// !(cell.classList.contains(‘number’))
-// })
-
-// function findSecondRandom(firstRandom) {
-// secondRandom = random()*cells.length
-// if (secondRandom === firstRandom) {
-// findSecondRandom(firstRandom)
-// return
-// } else { return secondRandom }
-
-// randomIndex = random()*cells.length
-// cells[randomIndex].classList.add(‘number’)  number class should have value (for 2,4,8,16,32,64,128,256,512,1024,2048)
-// cells[randomIndex].innerHTML = 2
-// randomIndex2 = findSecondRandom(randomIndex)
-// cells[randomIndex2].classList.add(‘number’)
-// cells[randomIndex2].innerHTML = 2
-
-// let movement = false
-// let arrayOfFreePos = []
-
-// document.addEventListener('keydown', (event) => {
-// const key = event.key
-// if (key === 'up’) {
-// -------------- inside foor-loop
-// for-loop (repeating width times)
-//  Indexes 0,1,2,3
-// compareTwoTilesForButtonUp(index)
-
-// function compareTwoTilesForButtonUp(startIndex)
-
-// check if border reached
-// if ( !(startIndex > (width ** 2) – width - 1) )
-// return
-
-// if (index0 true && index+4 true)
-
-// if (number.innerHTML of index+4 with index0 are same)
-// put together: remove index4 number class and change index0 number class (id++, innerHTML*2)(innerHTML*2)  can be linked to number class? “class / HTML presets?“
-// movement = true
-// return compareTwoTiles(startIndex)
-// else: return compareTwoTile(startIndex+=4) // (two different numbers), continue
-// else if (index0 false && index+4 true)
-// remove index+4 number class, save its value (valueNo), add to index0 number class with value valueNo
-// movement = true
-// if ( !(startIndex < width) )
-// return compareTwoTile(startIndex-=4)
-// else return compareTwoTile(startIndex+=4) 
-
-// // ! code below can be reduced to
-// // * else return compareTwoTile(startIndex+=4)
-
-// // else if (index0 true && index+4 false)
-// // return compareTwoTile(startIndex++)
-
-// // else if (index0 false && index+4 false) 
-// // return return compareTwoTile(startIndex++) 
-// -------------- out of foor-loop
-
-// checkIf2048()
-
-// if (movement) // check if there was movement (class Adding / Removing) over all columns getFreeTileIndexes()
-// if (arrayOfFreePos.length !== 0) // if arrayOfFreePos is not empty
-// randomIndex = arrayOfFreePos[random()] // Randomly pick one of the index elements within arrayOfFreePos
-// cells[randomIndex].classList.add(‘number’)
-// cells[randomIndex].innerHTML = 2
-// movement = false // reset movement
-// else // no movement
-// if (possibility === true)
-// Return
-// Else // possibility false
-// Alert(gameover)
